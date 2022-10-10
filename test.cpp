41a9f917c65c156b468616a7c90c2c2e663bcd2b
dlisitsin <lisicindm@gmail.com>
2022-10-10 09:22:50 +0300

    a few fixtures applied to the task wk_03

--- test.cpp
+++ test.cpp
@@ -5,49 +5,104 @@
 
 using namespace std;
 
-class condition {
-public: float xi, yi, omega;
+class State{
+public:
+    float x, v;
+};
 
-condition(float y, float x, float omega): yi(y), xi(x), omega(omega) {}
+class Solver {
+public: 
+    float xi, yi, omega;
+
+    State state;
 
 
-condition evolution (float dt) {
-    condition cj (yi - dt * xi * omega, xi + dt * yi, omega);
-    return cj;
-}
+    Solver(float y, float x, float omega): yi(y), xi(x), omega(omega) {
+        state.v = y;
+        state.x = x;
+    }
+
+
+    Solver evolution (float dt) {
+        // Solver cj (yi - dt * xi * omega, xi + dt * yi, omega);
+        // return cj;
+        return Solver {yi - dt * xi * omega, xi + dt * yi, omega};
+    }
+
+    void euler_step (float dt){
+        float v = state.v - dt * state.x * omega;
+        float x = state.x + dt * state.v;
+        this->state.x = x;
+        this->state.v = v;
+    }
 
 };
 
 
-void data(int n, float x0, float y0, float omega, float dt) {
+void solve(int n, float x0, float y0, float omega, float dt) {
 
-    condition c0 (x0, y0, omega);
+    Solver c0 (x0, y0, omega);
 
-    vector<condition> d;
+    vector<State> d;
 
-    d.push_back(c0);
+    d.push_back(c0.state);
     for (int i = 1; i < n; ++i) {
-        condition c1 = c0.evolution(dt);
-        d.push_back(c1);
-        c0 = c1;
+        // condition c1 = c0.evolution(dt);
+        c0.euler_step(dt);
+        d.push_back(c0.state);
+        // c0 = c1;
         // condition::evolution(d(i-1), omega, deltat);
     }
 
-    ofstream fout("03_10.txt");
-    for (int i = 0; i < n; ++i) {
-        fout << d[i].xi << " " << d[i].yi << endl;
+    {
+        ofstream fout("03_10.txt");
+        for (auto &el: d) {
+        // int i = 0; i < n; ++i) {
+            fout << el.x << " " << el.v << endl;
+        }
+        fout.close();
     }
-    fout.close();
-
 }
 
-int main() {
+#include <array>
+int main(int argc, char *argv[]) {
     long n;
-    cin >> n;
+    n = 10000;
+    // cin >> n;
     float x0, y0, omega, dt;
-    cin >> x0 >> y0 >> omega >> dt;
+    x0 = 64e5;
+    y0 = 0.0;
+    omega = 1;
+    dt = 1.0e-3;
 
-    data (n, x0, y0, omega, dt);
+    if (argc == 9){
+        std::cout << "using cmd arguments:" << std::endl;
+        for(int el=0; el<9; el++){
+            std::cout << argv[el]<< ", ";
+        }
+        for(int el=1; el<9; el+=2){
+            if(std::string(argv[el])=="-x"){
+                x0 = atof(argv[el+1]);
+            }
+            if(std::string(argv[el])=="-v"){
+                y0 = atof(argv[el+1]);
+            }
+            if(std::string(argv[el])=="-n"){
+                n = atol(argv[el+1]);
+            }
+            if(std::string(argv[el])=="-dt"){
+                dt = atof(argv[el+1]);
+            }
+        }
+    }
+    else {
+        std::cout << "using default initial state as  number of arguments != 9" << std::endl;
+
+    }
+    // cin >> x0 >> y0 >> omega >> dt;
+
+
+    solve (n, x0, y0, omega, dt);
 
 }
 
